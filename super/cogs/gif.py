b8c0eb9ac82c04d4fb6216c241c8461b7111a6d3
Simone Esposito <chaufnet@gmail.com>
2017-06-17 14:05:02 +0200

    cogs/gif: Use global aiohttp session

--- super/cogs/gif.py
+++ super/cogs/gif.py
@@ -7,37 +7,39 @@ import traceback
 class Gif:
     def __init__(self, bot):
         self.bot = bot
+        self.session = aiohttp.ClientSession()
+    
+    def __exit__(self):
+        self.session.close()
 
 
     async def _url_valid(self, url):
         try:
-            async with aiohttp.ClientSession() as session:
-                async with session.get(url, timeout=1) as resp:
-                    if resp.status < 400:
-                        return True
+            async with self.session.get(url, timeout=1) as resp:
+                if resp.status < 400:
+                    return True
         except:
             traceback.print_exc()
         return False
 
     async def _get_url(self, text, nsfw):
-        async with aiohttp.ClientSession() as session:
-            async with session.get(
-                'https://api.gifme.io/v1/search',
-                data=dict(key='rX7kbMzkGu7WJwvG', query=text, nsfw=nsfw),
+        async with self.session.get(
+            'https://api.gifme.io/v1/search',
+            data=dict(key='rX7kbMzkGu7WJwvG', query=text, nsfw=nsfw),
+            timeout=5,
+        ) as resp:
+            data = await resp.json()
+        for image in data['data']:
+            if await self._url_valid(image['link']):
+                return image['link']
+
+            async with self.session.post(
+                'https://rightgif.com/search/web',
+                data={'text': text},
                 timeout=5,
             ) as resp:
                 data = await resp.json()
-            for image in data['data']:
-                if await self._url_valid(image['link']):
-                    return image['link']
-
-                async with session.post(
-                    'https://rightgif.com/search/web',
-                    data={'text': text},
-                    timeout=5,
-                ) as resp:
-                    data = await resp.json()
-                    return data['url']
+                return data['url']
 
     @commands.command(no_pm=True, pass_context=True)
     async def gif(self, ctx):
